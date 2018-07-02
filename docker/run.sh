e87f18ce6a9a09f0f1242d3706012b900c9d2e7f
Conor Schaefer <conor.schaefer@gmail.com>
2018-07-01 20:19:50 -0700

    trims down dockerfile

--- docker/run.sh
+++ docker/run.sh
@@ -6,21 +6,21 @@ set -o pipefail
 if [[ -z "${RELAY_ADDRESS}" ]]
 then
     echo "RELAY_ADDRESS not defined"
-    exit 1
+    sleep 5
 fi
 
-for relaytype in bridge middle exit; do
-	sed -i 's/${RELAY_NICKNAME}/'"$RELAY_NICKNAME"'/g' "/etc/tor/torrc.$relaytype"
-	sed -i 's/${RELAY_ADDRESS}/'"$RELAY_ADDRESS"'/g' "/etc/tor/torrc.$relaytype"
-	sed -i 's/${CONTACT_GPG_FINGERPRINT}/'"$CONTACT_GPG_FINGERPRINT"'/g' "/etc/tor/torrc.$relaytype"
-	sed -i 's/${CONTACT_NAME}/'"$CONTACT_NAME"'/g' "/etc/tor/torrc.$relaytype"
-	sed -i 's/${CONTACT_EMAIL}/'"$CONTACT_EMAIL"'/g' "/etc/tor/torrc.$relaytype"
-	sed -i 's/${RELAY_BANDWIDTH_RATE}/'"$RELAY_BANDWIDTH_RATE"'/g' "/etc/tor/torrc.$relaytype"
-	sed -i 's/${RELAY_BANDWIDTH_BURST}/'"$RELAY_BANDWIDTH_BURST"'/g' "/etc/tor/torrc.$relaytype"
-	sed -i 's/${RELAY_PORT}/'"$RELAY_PORT"'/g' "/etc/tor/torrc.$relaytype"
-done
+torrc_file="/etc/tor/torrc.${RELAY_TYPE}"
+
+sed -i 's/${RELAY_NICKNAME}/'"$RELAY_NICKNAME"'/g' "$torrc_file"
+sed -i 's/${RELAY_ADDRESS}/'"$RELAY_ADDRESS"'/g' "$torrc_file"
+sed -i 's/${CONTACT_GPG_FINGERPRINT}/'"$CONTACT_GPG_FINGERPRINT"'/g' "$torrc_file"
+sed -i 's/${CONTACT_NAME}/'"$CONTACT_NAME"'/g' "$torrc_file"
+sed -i 's/${CONTACT_EMAIL}/'"$CONTACT_EMAIL"'/g' "$torrc_file"
+sed -i 's/${RELAY_BANDWIDTH_RATE}/'"$RELAY_BANDWIDTH_RATE"'/g' "$torrc_file"
+sed -i 's/${RELAY_BANDWIDTH_BURST}/'"$RELAY_BANDWIDTH_BURST"'/g' "$torrc_file"
+sed -i 's/${RELAY_PORT}/'"$RELAY_PORT"'/g' "$torrc_file"
 
 echo "Dumping torrc..."
-cat /etc/tor/torrc.${RELAY_TYPE}
+cat "$torrc_file"
 
-exec tor -f /etc/tor/torrc.${RELAY_TYPE}
+exec tor -f "$torrc_file"
